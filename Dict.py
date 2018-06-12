31ab38c0ee407039fa3eaf3001f4adb97d2cc852
Santosh Kumar <Singh.santosh2702@gmail.com>
2018-06-12 20:46:08 +0530

    Update Dict.py

--- Dict.py
+++ Dict.py
@@ -1,12 +1,6 @@
-Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> >>> dict={'Name':'sam','class':'what'}
+>>> dict={'Name':'sam','class':'what'}
 >>> print(dict)
 {'Name': 'sam', 'class': 'what'}
->>> del(class)
-SyntaxError: invalid syntax
->>> del dict{'Name'};
-SyntaxError: invalid syntax
 >>> del dict['Name'];
 >>> dict.clear();
 >>> del dict;
@@ -38,8 +32,7 @@ dict_items([('Name', 'sam'), ('class', 'what')])
 	
 Name sam
 class what
->>> for n in dict.items()
-SyntaxError: invalid syntax
+
 >>> for n in dict.items():
         print(n)        
 ('Name', 'sam')
