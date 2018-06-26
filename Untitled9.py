732d606b0b141ce5c792463ce1c490424120d93c
sam2702 <Singh.santosh2702@gmail.com>
2018-06-27 01:44:50 +0530

    commit

---
+++ Untitled9.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# ##  Server
+
+# In[ ]:
+
+
+import socket
+s=socket.socket()
+host=socket.gethostname()
+port=8000
+s.bind((host,port))
+print("waiting for connection...........")
+s.listen(5)
+while True:
+    conn,addr=s.accept()
+    print('GOT Connection from',addr)
+    #conn.send("Server saying hi")
+    conn.close()
+
+
+# ## client
+
+# In[ ]:
+
+
+import socket
+s=socket.socket()
+host=socket.gethostname()
+port=8000
+s.connect((host,port))
+print(s.recv(1024).decode())
+s.close()
+
