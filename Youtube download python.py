caeec12081e85c533c860ab54071cb45b06a2890
Santosh Kumar <Singh.santosh2702@gmail.com>
2018-08-06 23:35:22 +0530

    commit

--- Youtube download python.py
+++ Youtube download python.py
@@ -1,15 +1,3 @@
-##import cv2
-##import numpy as np
-##cap=cv2.VideoCapture('Alan.mp4')
-##
-##
-##
-##while(1):
-##    _,frame=cap.read()
-##    cv2.imshow('frame',frame)
-
-##   TO DOWNLOAD A VIDEO FROM YOUTUBE USING PYTHON
-##
 import pytube
 link="https://www.youtube.com/watch?time_continue=4&v=ChA_zph7aao"
 yt=pytube.YouTube(link)
